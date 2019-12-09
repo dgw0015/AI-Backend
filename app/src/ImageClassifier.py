0e36adf1f8842e4a4d2eef0a5d0bc56a60b92cf5
Drew Waller <drewwaller67@gmail.com>
2019-12-08 23:33:15 -0600

    Update ImageClassifier.py

--- app/src/ImageClassifier.py
+++ app/src/ImageClassifier.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle as pkl
 import os
 import matplotlib.pyplot as plt
+import cv2
 from keras.applications import VGG16
 from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
@@ -15,46 +16,67 @@ from keras.utils import np_utils
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-train_datagen=ImageDataGenerator(rescale=1./255., preprocessing_function=preprocess_input) #included in our dependencies
-
-train_generator=train_datagen.flow_from_directory('dog vs cat/dataset/train',
-                                                 target_size=(150,150),
-                                                 color_mode='rgb',
-                                                 batch_size=16,
-                                                 class_mode='categorical')
-
-
 num_classes = 11
 model_name = 'imageClassifier.h5'
+categories = ["Dog", "Cat", "Mountain", "Beach", "Bird", "Forest", "Waterfall", "Tiger", "Flower", "Horse", "Castle"]
+
+img_array =
+new_array = cv2.resize()
+
+train_datagen = ImageDataGenerator(rescale=1. / 255., preprocessing_function=preprocess_input)
+train_generator = train_datagen.flow_from_directory('C:/Users/Drew Waller/Documents/AI-Backend/app/data/images/train',
+                                                    target_size=(150, 150),
+                                                    color_mode='rgb',
+                                                    batch_size=25,
+                                                    class_mode="categorical")
+
+valid_datagen = ImageDataGenerator(rescale=1. / 255., preprocessing_function=preprocess_input)
+valid_generator = valid_datagen.flow_from_directory('C:/Users/Drew Waller/Documents/AI-Backend/app/data/images/valid',
+                                                    target_size=(150, 150),
+                                                    color_mode='rgb',
+                                                    batch_size=25,
+                                                    class_mode="categorical")
 
 model = Sequential()
-
-model.add(Conv2D(128, (3, 3), padding='same'))
-model.add(Activation('elu'))
-model.add(Conv2D(128, (3, 3)))
-model.add(Activation('elu'))
+model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, (3, 3), padding='same'))
-model.add(Activation('elu'))
-model.add(Conv2D(256, (3, 3)))
-model.add(Activation('elu'))
+model.add(Conv2D(32, (3, 3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('elu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
+model.add(Dense(64, activation="relu"))
+model.add(Dense(2, activation="softmax"))
 
 model.summary()
 
-opt = SGD(lr=learning_rate, momentum=momentum_rate, decay=decay_rate)
+opt = SGD(lr=0.01, momentum=0.9, decay=0.00005)
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 ckeckpoint = ModelCheckpoint(filepath=model_name, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks = [ckeckpoint]
+
+model.fit_generator(train_generator,
+                    epochs=10,
+                    validation_data=valid_generator)
+
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(150, 150, 3))
+
+base_model.summary()
+
+tl_model = Sequential()
+tl_model.add(base_model)
+tl_model.add(Flatten())
+tl_model.add(Dense(2, activation="softmax"))
+
+tl_model.summary()
+
+history = tl_model.fit_generator(train_generator,
+                                 epochs=10,
+                                 validation_data=valid_generator,
+                                 callbacks=callbacks)
 
 # plot loss during training
 plt.title('Loss')
@@ -69,7 +91,3 @@ plt.plot(history.history['accuracy'], 'b', label='train')
 plt.plot(history.history['val_accuracy'], 'g', label='test')
 plt.legend()
 plt.show()
-
-
-
-
